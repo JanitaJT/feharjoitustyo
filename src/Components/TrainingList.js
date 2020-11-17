4e78395b2cb4559194b66ed72689ec769e2a1cc4
Janita <bgj369@myy.haaga-helia.fi>
2020-11-17 12:19:59 +0200

    Date saatu muokattua

--- src/Components/TrainingList.js
+++ src/Components/TrainingList.js
@@ -2,32 +2,44 @@ import React, {useState, useEffect} from 'react'
 import {AgGridReact} from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
-import 'moment';
+import moment from  'moment';
 
 export default function TrainingList() {
 
     const [trainings, setTrainings] = useState([]);
+  
+    
+    
 
     useEffect(() => {
         getTrainings();
+        
     }, [])
 
     const getTrainings = () => {
         fetch("https://customerrest.herokuapp.com/api/trainings")
         .then(response => response.json())
-        .then(data => setTrainings(data.content))
+        .then(data => {
+            
+            setTrainings(data.content)})
         .catch(err => console.error(err))
     }
 
     const columns = [
-        {headerName: 'Date', field: 'date', sortable: true, filter: true},
+       
+        {headerName: 'Date', field: 'date', sortable: true, filter: true, valueFormatter: dateFormatter},
         {headerName: 'Duration', field: 'duration', sortable: true, filter: true},
         {headerName: 'Activity', field: 'activity', sortable: true, filter: true},
     ]
 
+    function dateFormatter(params) {
+        return moment(params.value).locale('fi').format("Do MMM YY"); 
+      }
+
     return (
         <div className="ag-theme-material" style={{height: '700px', width: '100%', margin: 'auto'}}>
         <AgGridReact
+        
         columnDefs={columns}
         rowData={trainings}
         >
